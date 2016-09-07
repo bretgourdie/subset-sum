44b7993aa5e5791e9d411f3b2121a2a94fadaa66
Bret Gourdie <bretgourdie@gmail.com>
2016-09-07 09:58:12 -0400

    Extracted InvalidInput to new file

---
+++ Subset Sum Calculator/InvalidInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subset_Sum_Calculator
+{
+    /// <summary>
+    /// The state of the input on the form.
+    /// </summary>
+    public enum InvalidInput
+    {
+        /// <summary>
+        /// Denotes the "sum" field is invalid.
+        /// </summary>
+        Sum, 
+        /// <summary>
+        /// Denotes the "values" field is invalid.
+        /// </summary>
+        Values, 
+        /// <summary>
+        /// Denotes that no "values" sum together to the "sum" field.
+        /// </summary>
+        NoResults,
+        /// <summary>
+        /// Denotes that no inputs are invalid.
+        /// </summary>
+        None
+    }
+}
--- Subset Sum Calculator/MainForm.cs
+++ Subset Sum Calculator/MainForm.cs
@@ -237,27 +237,4 @@ namespace Subset_Sum_Calculator
             this.Close();
         }
     }
-
-    /// <summary>
-    /// The state of the input on the form.
-    /// </summary>
-    public enum InvalidInput
-    {
-        /// <summary>
-        /// Denotes the "sum" field is invalid.
-        /// </summary>
-        Sum, 
-        /// <summary>
-        /// Denotes the "values" field is invalid.
-        /// </summary>
-        Values, 
-        /// <summary>
-        /// Denotes that no "values" sum together to the "sum" field.
-        /// </summary>
-        NoResults,
-        /// <summary>
-        /// Denotes that no inputs are invalid.
-        /// </summary>
-        None
-    }
 }
